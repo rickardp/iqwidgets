a50b46cfdad52427a8c6390093c96e1ed3042c69
Rickard Petzäll <rickard@evolviq.com>
2011-04-29 17:59:37 +0200

    Before merge

---
+++ IQViewTessellation.h
@@ -0,0 +1,16 @@
+//
+//  IQViewTessellation.h
+//  IQWidgets
+//
+//  Created by Rickard Petzäll on 2011-04-12.
+//  Copyright 2011 EvolvIQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IQViewTessellation : NSObject {
+    
+}
+
+@end
---
+++ IQViewTessellation.m
@@ -0,0 +1,14 @@
+//
+//  IQViewTessellation.m
+//  IQWidgets
+//
+//  Created by Rickard Petzäll on 2011-04-12.
+//  Copyright 2011 EvolvIQ. All rights reserved.
+//
+
+#import "IQViewTessellation.h"
+
+
+@implementation IQViewTessellation
+
+@end
