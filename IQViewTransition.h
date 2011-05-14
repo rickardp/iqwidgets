bb0f8caf096e0f69f987f07dc9d0853fd41a6641
Rickard Petzäll <rickard@evolviq.com>
2011-05-14 18:40:10 +0200

    Fixed memory management for IQViewTessellation/IQViewTransition

---
+++ IQViewTransition.h
@@ -0,0 +1,16 @@
+//
+//  IQViewTransition.h
+//  IQWidgets
+//
+//  Created by Rickard Petzäll on 2011-05-11.
+//  Copyright 2011 EvolvIQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IQViewTransition : NSObject {
+    
+}
+
+@end
---
+++ IQViewTransition.m
@@ -0,0 +1,14 @@
+//
+//  IQViewTransition.m
+//  IQWidgets
+//
+//  Created by Rickard Petzäll on 2011-05-11.
+//  Copyright 2011 EvolvIQ. All rights reserved.
+//
+
+#import "IQViewTransition.h"
+
+
+@implementation IQViewTransition
+
+@end
